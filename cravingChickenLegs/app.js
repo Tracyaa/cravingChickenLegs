a2422b720f22d1c6b2f04a8744d7e27ff227fb11
github username <github email address>
2019-03-28 11:26:05 -0400

    merging branch to master

---
+++ cravingChickenLegs/app.js
@@ -0,0 +1,26 @@
+PIXI.utils.sayHello();
+
+const displayDiv = document.querySelector('#display')
+const timerHeader = document.querySelector("#timer")
+const renderer = PIXI.autoDetectRenderer(500, 500, {
+    transparent: true,
+    resolution: 1
+});
+const stage = new PIXI.Container();
+
+displayDiv.appendChild(renderer.view);
+
+// const loadImages = () => {
+PIXI.loader
+    .add("mainMenuBg", "images/9046OT_06_02.jpg")
+    .add("skeleton", "images/BODY_skeleton.png")
+    .add("drumStick", "images/Pataepollo.png")
+    .add("chicken", "images/chicken.png")
+    .add("greenBg", "images/background1.png")
+// }
+
+
+function animationLoop() {
+    requestAnimationFrame(animationLoop);
+    renderer.render(stage);
+};
---
+++ cravingChickenLegs/gameStart.js
@@ -0,0 +1,29 @@
+const gameStartText = new PIXI.Text('Times up!', {
+    fontFamily: 'Times New Roman',
+    fontSize: 36,
+    fill: 0xff1010,
+    align: 'center'
+});
+
+const gameStart = () => {
+    PIXI.loader
+        .load(bgSetup)
+        .load(skeletonSetup)
+        .load(chickenSetup)
+        .load(drumStickSetup)
+
+    gameTimer();
+};
+
+let gameTime = 10;
+const gameTimer = () => {
+    setInterval(function() {
+        timerHeader.innerText = `${gameTime} Seconds`;
+        gameTime -= 1;
+        if (gameTime <= -1) {
+            gameOver();
+        }
+    }, 1000);
+};
+
+gameStart();
