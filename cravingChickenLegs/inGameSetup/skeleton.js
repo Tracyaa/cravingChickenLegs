f73b7c747463baa9cc43a3fb7f7c1951e1981671
Tracyaa <tracyho0867@gmail.com>
2019-03-29 13:34:18 -0400

    fixed keycode

--- cravingChickenLegs/inGameSetup/skeleton.js
+++ cravingChickenLegs/inGameSetup/skeleton.js
@@ -82,7 +82,6 @@ const ateDrumpStick = () => {
     if (legBounds.x + (legBounds.width + 14) > (skeletonSpriteBounds.x + 28) && legBounds.x < skeletonSpriteBounds.x + (skeletonSpriteBounds.width - 28) && legBounds.y + (legBounds.height + 14) > (skeletonSpriteBounds.y + 30) && legBounds.y < skeletonSpriteBounds.y + skeletonSpriteBounds.height) {
       skeletonTotalScore += 10;
       skeletonScoreBoard.innerText = `Score: ${skeletonTotalScore}`
-      console.log(skeletonTotalScore);
       randomRelocate();
       respondTime = 10;
     };
