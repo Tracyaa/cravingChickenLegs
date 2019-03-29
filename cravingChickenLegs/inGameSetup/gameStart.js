ce74c94fcd803b483bf08147e3797084d1852b5e
github username <github email address>
2019-03-29 12:03:06 -0400

    made some styling changes

--- cravingChickenLegs/inGameSetup/gameStart.js
+++ cravingChickenLegs/inGameSetup/gameStart.js
@@ -1,19 +1,18 @@
 const startGameDiv = document.querySelector("#top-text")
 const gameStart = () => {
-  PIXI.loader
-    .load(bgSetup)
-    .load(skeletonSetup)
-    .load(chickenSetup)
-    .load(drumStickSetup)
-  gameTimer();
-  respondTimer()
+    PIXI.loader
+        .load(bgSetup)
+        .load(skeletonSetup)
+        .load(chickenSetup)
+        .load(drumStickSetup)
+    gameTimer();
+    respondTimer()
 };
 
 
 
 let gameTime = 60;
 const gameTimer = () => {
-<<<<<<< HEAD
     const idle = setInterval(function() {
         timerDiv.innerHTML = `<h2>${gameTime} Seconds</h2>`;
         gameTime -= 1;
@@ -23,9 +22,6 @@ const gameTimer = () => {
             updateScoreApi(parseInt(skeletonForm.dataset.id), skeletonTotalScore)
             clearInterval(idle)
             gameOver();
-            // debugger
-            // debugger
-
         }
     }, 1000);
 };
@@ -39,49 +35,15 @@ window.addEventListener('keydown', event => {
         }
     }
 })
-=======
-  const idle = setInterval(function() {
-    timerDiv.innerHTML = `<h2>${gameTime} Seconds</h2>`;
-    gameTime -= 1;
-    if (gameTime <= -1) {
-      reloadDiv.innerHTML = `<input type="button" value="Play again?" onClick="document.location.reload(true)" class="btn btn-warning">`
-      updateScoreApi(parseInt(chickenForm.dataset.id), chickenTotalScore)
-      updateScoreApi(parseInt(skeletonForm.dataset.id), skeletonTotalScore)
-      clearInterval(idle)
-      gameOver();
-    }
-  }, 1000);
-};
-
-
-startGameDiv.innerHTML = `<h2 class="center blink_me">click here to start</h2>`
-
-startGameDiv.addEventListener('click', event => {
-  if (isGameStart === false) {
-    gameStart();
-    gameTime = 60;
-    isGameStart = true;
-  }
-});
->>>>>>> master
-
-
-
-
 
 const updateScore = (playerId, playerScore) => {
-  return fetch(`http://localhost:3000/players/${playerId}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({
-        score: playerScore,
-    })
-<<<<<<< HEAD
-}
-=======
-  });
-};
->>>>>>> master
+        return fetch(`http://localhost:3000/players/${playerId}`, {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json"
+                    },
+                    body: JSON.stringify({
+                        score: playerScore,
+                    })
+                }
